f1b3db218ad73c16360b69f7fcc974d5611708ea
Adithi M <adithimuralidhara@gmail.com>
2024-01-31 10:23:02 +0530

    update

---
+++ quiz/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import First from './component/first.jsx';
+import Second from './component/second.jsx';
+import Third from './component/third.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <First/>
+      <Second/>
+      <Third/>
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz/src/component/first.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './first.css'
+
+export default class First extends Component {
+  render() {
+    return (
+      <div class='home'>
+        <h1 class='quiz'>Quiz App</h1>
+        <button class="play">Play</button>
+      </div>
+    )
+  }
+}
